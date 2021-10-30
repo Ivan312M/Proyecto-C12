d0ef54d92dd4601ed94ce76dab208584a6f7403e
Ivan312M <93168382+Ivan312M@users.noreply.github.com>
2021-10-29 18:34:50 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -46,7 +46,17 @@ function draw() {
  var select_sprites = Math.round(random(1,3));
 
   
-   if (frameCount % 50 == 0) {
+  // if (frameCount % 50 == 0) {
+  //  if (select_sprites == 1) {
+  //    createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //    createRed();
+  //   }
+  // }
+
+   if (frameCount % 80 == 0) {
      if (select_sprites == 1) {
        createApples();
      } else if (select_sprites == 2) {
@@ -56,19 +66,12 @@ function draw() {
      }
    }
 
-  // if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }
-  // }
-
   //   if (frameCount / 80 == 0) {
   //   if (select_sprites == 1) {
-  //    createApples();
+   //   createApples();
   //   } else if (select_sprites == 2) {
   //     createOrange();
+  //   }else {
   //   }else {
   //     createRed();
   //   }
@@ -80,7 +83,7 @@ function draw() {
   //   } else if (select_sprites == 2) {
   //     createOrange();
   //   }else {
-  //     createRed();
+   //    createRed();
   //   }
   // }
 
